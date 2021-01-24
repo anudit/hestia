16fc0189905c0a00e136cf395e34c9527d5b998c
Anudit Nagar <nagaranudit@gmail.com>
2021-01-24 15:43:33 +0530

    Chainlink Setup

---
+++ hardhat.config.js
@@ -0,0 +1,57 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require('dotenv').config()
+
+let mnemonic = process.env.MNEMONIC;
+
+const infuraNetwork = (network, chainId, gas) => {
+  return {
+    url: `https://${network}.infura.io/v3/${process.env.PROJECT_ID}`,
+    chainId,
+    gas,
+    accounts: mnemonic ? { mnemonic } : undefined
+  };
+};
+
+module.exports = {
+  solidity: {
+    version: "0.7.6",
+    settings: {
+      optimizer: {
+        runs: 999999,
+        enabled: true
+      }
+    }
+  },
+  networks: {
+    hardhat: mnemonic ? { accounts: { mnemonic } } : {},
+    local: {
+      url: "http://localhost:8545",
+      accounts: mnemonic ? { mnemonic } : undefined
+    },
+    rinkeby: infuraNetwork("rinkeby", 4, 6283185),
+    kovan: infuraNetwork("kovan", 42, 6283185),
+    goerli: infuraNetwork("goerli", 5, 6283185),
+    mumbai: {
+      url: "https://rpc-mumbai.matic.today",
+      chainId: 80001,
+      accounts: mnemonic ? { mnemonic } : undefined
+    },
+    bsc_testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      accounts: mnemonic ? { mnemonic } : undefined
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY ? process.env.ETHERSCAN_API_KEY : undefined
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 1,
+    coinmarketcap: process.env.CMC_APIKEY
+  }
+};
+
